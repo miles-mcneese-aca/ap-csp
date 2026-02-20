0012e2f7e3a7531e04aa0f67131ab461e17016ed
Miles McNeese <miles.mcneese@student.atlantaclassical.org>
2026-02-20 14:17:19 -0500

    Added gurt.py (Version D)

---
+++ gurt.py
@@ -0,0 +1,28 @@
+var = 'logic'
+print(var)
+count = 0
+print(count)
+print(count+1)
+r = 11
+s = 2
+print(r-s)
+print(r%s)
+base=4
+print(base^3)
+
+subject = 'Computer'
+category = 'Science'
+print(subject + " " + category)
+x = 45
+y = 50
+print(x!=y)
+print(x>50)
+in_stock = True
+on_sale = True
+print(in_stock and on_sale)
+print(10+2>11)
+depth=15
+width=10
+height=2
+volume = depth*width*height
+print(f"Depth: {depth}, Width: {width}, Height: {height}, Volume: {volume}")
